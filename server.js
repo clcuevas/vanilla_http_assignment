268945beb0232f25e70b75424a91136cfa18db97
Claudia Cuevas <omordn@gmail.com>
2015-05-05 11:15:37 -0700

    add working server.js file with minor /greet shows undefined bug

--- server.js
+++ server.js
@@ -1,17 +1,47 @@
 'use strict';
 
 var http = require('http');
+var url = require('url');
 
 var server = http.createServer(function(request, response) {
-  if(request.url === '/time') {
+  var userInput = url.parse(request.url).pathname.split('/');
+  var getSingleWord = userInput[2];
+  var getPath = userInput[1];
+
+  if(('/' + getPath) === '/time') {
     //do something with /time request here
-  } else if(request.url === '/greet') {
+    var date = new Date().toString();
+    response.writeHead(200, {
+      "Content-Type": "text/plain"
+    });//end /time 200 write header
+    response.write('Server Date: ' + date);
+    return response.end();
+  } else if(('/' + getPath) === '/greet') {
+    console.log('hit greet path');
     //do something with /greet request here
+    response.writeHead(200, {
+      "Content-Type": "application/json, text/plain"
+    });//end /greet write head
+
+    if(request.method === 'POST') {
+      request.on('data', function(data) {
+        //var pathname = url.parse(request.url).data;
+        response.write(JSON.stringify({msg: 'hello world'}));
+        return response.end();
+      });//end on POST request
+    } else {
+      response.write(JSON.stringify({msg: 'hello ' + getSingleWord}));
+      return response.end();
+    }
   } else {
     response.writeHead(404, {
-      "Content-Type": "text/plain"
+      "Content-Type": "application/json"
     });//end 404 write header
-    response.write('404 Page Not Found');
-    response.end();
+    response.write(JSON.stringify({msg: '404 Page Not Found'}));
+    return response.end();
   }
 });//end server create
+
+server.listen(3000, function() {
+  console.log('Server has started');
+});
